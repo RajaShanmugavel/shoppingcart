c4a3b7177320eeb35ee98fb70afae69149c141f5
RajaShanmugavel <raja.karupiah@gmail.com>
2017-12-30 17:14:01 +1000

    misc updates to recipe list

--- cartapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cartapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test','../../Recipe-Cooking.jpg')
+    new Recipe('Recipe 1', 'This is simply a test','https://c1.staticflickr.com/9/8346/8258253775_d8fa9aa697_b.jpg'),
+    new Recipe('Recipe 2', 'This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM_sRXmjAX8jmBH8daGPssaylbN2uNixTcWfSzbX7KHP_iKbfOFg'),
+    new Recipe('Recipe 3', 'This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxrrfBmVYr6eyEN5W0XwYAJkOeLw7P-yrlrP-SCVHmRROeFVRpAQ')
   ];
 
   constructor() { }
