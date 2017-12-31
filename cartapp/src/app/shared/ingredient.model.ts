20730a0d82d8fbaee8189a0f5b82ec6caa7a6bdc
RajaShanmugavel <raja.karupiah@gmail.com>
2017-12-31 15:23:27 +1000

    updates to component ts

---
+++ cartapp/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient{
+    constructor(public name:string, public amount:number){};
+
+    // => The above does the same as below lines
+    // public name: string;
+    // public amount:number;
+    
+
+    // constructor(name:string, amount:number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+}
