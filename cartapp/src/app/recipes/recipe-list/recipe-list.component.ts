8ab29d9678c75e0c22ef61490cc72d1fe3cdc295
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-15 19:18:59 +1000

    amends to shopping list items

--- cartapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cartapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Recipe 1', 'This is simply a test','https://c1.staticflickr.com/9/8346/8258253775_d8fa9aa697_b.jpg'),
     new Recipe('Recipe 2', 'This is simply a test','http://maxpixel.freegreatpicture.com/static/photo/1x/Sweets-Sweet-Indian-Food-Rasgula-Dessert-577230.jpg'),
-    new Recipe('Recipe 3', 'This is simply a test','https://cdn.pixabay.com/photo/2017/11/11/10/14/pasta-2938726_960_720.jpg')
+    new Recipe('Recipe 3', 'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/2/2e/Mmm..._pretty_summer_salad_%286005733739%29.jpg')
   ];
 
   constructor() { }
