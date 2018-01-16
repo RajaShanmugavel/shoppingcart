01398db0128a9d28ce50286ce6f4cc87cae94989
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-16 21:34:38 +1000

    comments

--- cartapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cartapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
+  recipes: Recipe[] = [ // These images are from internet with redistributable permission granted
     new Recipe('Recipe 1', 'This is simply a test','https://c1.staticflickr.com/9/8346/8258253775_d8fa9aa697_b.jpg'),
     new Recipe('Recipe 2', 'This is simply a test','http://maxpixel.freegreatpicture.com/static/photo/1x/Sweets-Sweet-Indian-Food-Rasgula-Dessert-577230.jpg'),
     new Recipe('Recipe 3', 'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/2/2e/Mmm..._pretty_summer_salad_%286005733739%29.jpg')
