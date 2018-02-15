c8992cd829c04243a5c95a8d41e28e4b8783ba12
RajaShanmugavel <raja.karupiah@gmail.com>
2018-02-15 21:21:27 +1000

    cart update to recipe service

--- cartapp/src/app/recipes/recipe.service.ts
+++ cartapp/src/app/recipes/recipe.service.ts
@@ -1,16 +1,26 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
-   private recipes: Recipe[] = [ // These images are from internet with redistributable permission granted
-        new Recipe('Recipe 1', 'This is simply a test', 'https://c1.staticflickr.com/9/8346/8258253775_d8fa9aa697_b.jpg'),
-        new Recipe('Recipe 2', 'This is simply a test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Sweets-Sweet-Indian-Food-Rasgula-Dessert-577230.jpg'),
-        new Recipe('Recipe 3', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Mmm..._pretty_summer_salad_%286005733739%29.jpg')
+    private recipes: Recipe[] = [ // These images are from internet with redistributable permission granted
+        new Recipe('Brownie',
+            'Delicious Brownie',
+            'https://c1.staticflickr.com/9/8346/8258253775_d8fa9aa697_b.jpg',
+            [new Ingredient('Sugar', 2), new Ingredient('Flour', 3),new Ingredient('Coco powder', 1)]),
+        new Recipe('Rasagula',
+            'Mouthwatering kolkatta rasagulla',
+            'http://maxpixel.freegreatpicture.com/static/photo/1x/Sweets-Sweet-Indian-Food-Rasgula-Dessert-577230.jpg',
+            [new Ingredient('Milk', 5), new Ingredient('Sugar', 2), new Ingredient('Flour', 1), new Ingredient('Coconut', 1)]),
+        new Recipe('Pasta',
+            'Prefectly made Italian Pasta',
+            'https://upload.wikimedia.org/wikipedia/commons/2/2e/Mmm..._pretty_summer_salad_%286005733739%29.jpg',
+            [new Ingredient('Pasta', 25), new Ingredient('Cheese', 2), new Ingredient('Cauliflower', 0.5), new Ingredient('Tomato', 5)])
     ];
 
-    getRecipes(){
-        return this.recipes.slice(); 
+    getRecipes() {
+        return this.recipes.slice();
     }
 
 }
